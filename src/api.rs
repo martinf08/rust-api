d86af099a81004dc25101d0a114b7eac04d7d2c6
Martin <martin.fontana08@gmail.com>
2021-11-28 19:57:09 +0100

    Setup pagination for products

--- src/api.rs
+++ src/api.rs
@@ -1,14 +1,22 @@
 use crate::db::*;
 
+use actix_web::web::Query;
 use actix_web::{web, HttpResponse};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Pagination {
+    pub page: Option<i32>,
+    pub limit: Option<i32>,
+}
 
 pub fn configure_api(cfg: &mut web::ServiceConfig) {
     cfg.service(web::resource("products").route(web::get().to(index)));
 }
 
-async fn index(pool: web::Data<Pool>) -> HttpResponse {
+async fn index(pool: web::Data<Pool>, params: Query<Pagination>) -> HttpResponse {
     let conn = pool.get().unwrap();
-    let products = get_products(conn);
+    let products = get_products(conn, params);
 
     HttpResponse::Ok().json(products)
 }
